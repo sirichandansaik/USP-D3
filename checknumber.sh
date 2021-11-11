2f871de2048619923647d5b1d69347c50f310976
sirichandansaik <71492795+sirichandansaik@users.noreply.github.com>
2021-11-11 10:31:06 +0530

    Add files via upload

---
+++ checknumber.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "enter the number "
+read number
+if test $number -eq 0;then
+echo "$number is 0"
+elif test $number -lt 0;then
+echo "number is negative"
+else
+echo "number is positive"
+fi
+
